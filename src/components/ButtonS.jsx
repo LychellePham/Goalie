fbb30e7646f1036af6c5d571068c5e4437db8f36
Lychelle Pham <lychelle.pham@ucalgary.ca>
2025-05-09 22:32:25 -0600

    first commit

---
+++ src/components/ButtonS.jsx
@@ -0,0 +1,8 @@
+export default function ButtonS(prop){
+    return(
+        <div className="bg-green-400 rounded-md m-8 p-16">
+            <p>{prop.words}</p>
+            HELLO
+        </div>
+    );
+}
