6dbc6a672d64983702a390e0ecc213ce9ed62a21
Lychelle Pham <lychelle.pham@ucalgary.ca>
2025-05-12 22:34:30 -0600

    add routing to project, so we can switch pages

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,19 @@
+export default function DashboardPage(){
+    // useEffect to fetch goals from backend
+
+    return(
+        <>
+        <div className="bg-beige">
+            <div className="bg-amber-400">
+                <h1 className="text-9xl">Goalie</h1>
+                <img src="/GOALIE2.png" alt="Goalie Logo"></img>
+                <h2>bum</h2>
+                <p>pee</p>
+
+            </div>
+
+        </div>
+        
+        </>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,31 @@
+'use client';
 import Image from "next/image";
-import ButtonS from "@/components/ButtonS";
+
+
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
-  return (
-    <>
+  const router = useRouter();
 
-        <p className="text-red-500">HIIII</p>
-        <ButtonS words="GET STARTED"></ButtonS>
-    </>
+  const changePage = () => {
+    router.push('/dashboard');
+  };
+
+  return (
+  
+    <div className="bg-beige min-h-screen">
+
+      <button onClick={changePage} className="cursor-pointer">Dashboard</button>
+    
+    
+
+        {/* <p className="text-red-500">HIIII</p>
+        <ButtonS words="GET STARTED"></ButtonS> */}
+    </div>
+
+        
+    
   );
   //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
   //     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
