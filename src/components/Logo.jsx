ac3b7ecd022d61df71a2f056dd6cb31f8f87b457
Lychelle Pham <lychellepham731@gmail.com>
2025-05-26 10:43:10 -0600

    test email change for git

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,5 +5,6 @@ export default function Logo(){
             <h1 className="w-fit text-9xl font-sans bg-amber-600">Goalie</h1>
             <img src="/GOALIE2.png" alt="Goalie Logo" className="h-70"></img>
         </div>
+        // hii
     );
 }
